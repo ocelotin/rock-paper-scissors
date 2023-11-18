4bf164b02b92336b5b9ecb0ae7ed19ced0182336
ocelotin <ignacio.siegel@gmail.com>
2023-11-18 19:39:58 -0300

    Add function playRound

--- javascript.js
+++ javascript.js
@@ -2,3 +2,16 @@ function getComputerChoice(){
     const CHOICE = ["rock", "paper", "scissors"];
     return CHOICE[Math.floor(Math.random() * 3)]; 
 }
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection;
+    computerSelection = computerSelection;
+    if (playerSelection === computerSelection){
+        return ("Draw!");
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+        playerSelection === "paper" && computerSelection === "rock" ||
+        playerSelection === "scissors" && computerSelection === "paper" ){
+            return (`You Win! ${playerSelection} beats ${computerSelection}`);
+        } else {
+            return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+        }
+}
